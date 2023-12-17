03d1bf7b08d9e5fa4d3c7f9ac8c25207f1d46dd8
Rohan Anil Tade <114643307+rohantade8@users.noreply.github.com>
2023-12-17 23:07:00 +0530

    Add files via upload

---
+++ 001.py
@@ -0,0 +1,13 @@
+#solution 1 with predefined variables
+num1 = 30
+num2 = 40
+
+print("the sum of given two number is",num1+num2)
+
+#solution 2 with user input
+num1 =float(input("enter a number here: "))
+num2 =float(input("enter another number here: "))
+
+sum = num1+num2
+
+print("the sum of the provided two numbrers is", sum)
---
+++ 002.py
@@ -0,0 +1,3 @@
+#to print hello world
+
+print("hello world")
